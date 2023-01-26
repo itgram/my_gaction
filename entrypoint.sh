90825fe9d093ec03e0ada810546acfa223acb022
elhussein <a_elhussein@hotmail.com>
2023-01-26 23:39:46 +0200

    my hello github action

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -l
+
+# https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action
+# chmod u+x ./entrypoint.sh
+
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
